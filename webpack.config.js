9ebfd5e84d50f38eb2c2ef52706557e37cd27c34
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-21 16:35:12 +0900

    모듈 및 룰 키 설정.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require("path");
 const config = {
     enrty:path.resolve(__dirname,"src","main.ts"),
     mode:"development",
+    module:{
+        rules:[]
+    },
     output:{
         filename: "main.js",
         path:path.resolve(__dirname, "dist")
