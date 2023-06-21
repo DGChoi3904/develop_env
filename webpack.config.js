ae317c8a691e1f71c61cd437342a37f67a39d9c9
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-21 16:32:40 +0900

    output설정, 루트/dist/main.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,9 @@ const path = require("path");
 
 const config = {
     enrty:path.resolve(__dirname,"src","main.ts"),
-    mode:"development"
+    mode:"development",
+    output:{
+        filename: "main.js",
+        path:path.resolve(__dirname, "dist")
+    }
 }
