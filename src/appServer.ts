773553e8de1eda0f5f4ec24401f25f428ef983e2
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-22 12:30:13 +0900

    index.html 파일 mainHtml에 할당, 응답에 첨부.

--- src/appServer.ts
+++ src/appServer.ts
@@ -4,7 +4,10 @@ import fs from "fs";
 const server = http.createServer((req,res)=> {    
     if(req.method === "GET"){
         if(req.url === "/"){
-            
+            const mainHtml = fs.readFileSync("./index.html");
+            res.writeHead(200,{"Content-type":"text/html"});
+            res.end(mainHtml);   
         }
     }
 })
+
